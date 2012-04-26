cbd33f2cf6975c560ca7ae7eb1e6d232fc252168
Yacobus Reinhart <yacobus.reinhart@gmail.com>
2012-04-26 16:25:21 +0000

    from branch

--- lib/finforenet/workers/analyze_keywords.rb
+++ lib/finforenet/workers/analyze_keywords.rb
@@ -5,14 +5,14 @@ module Finforenet
 			attr_accessor 
 			
 			def initialize(keywords, created_at, audience)
-				update_daily_tweet(keywords, created_at, audience)
+				update_daily_tweet(keywords.gsub(/^([^\$|\w]+)|(\s)$/,""), created_at, audience)
 			end
 			
 			def update_daily_tweet(keywords, created_at, audience)
 				keywords.split(",").each do |keyword|
 					regex_keyword = Finforenet::Utils::String.keyword_regex(keyword.gsub(/(^\s|\s$)/,""))
-					saved_keyword = Keyword.where({:title => /#{regex_keyword}/i}).first
-					DailyTweet.save_total_and_follower(created_at, followers, saved_keyword.id) if saved_keyword
+					saved_keyword = Keyword.where({:title => regex_keyword}).first
+					DailyTweet.save_total_and_follower(created_at, audience, saved_keyword.id) if saved_keyword
 				end
 			end
 
